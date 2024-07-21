201308513cbf05a6cfe7b4637ad02e951bd24e2b
saitejav <saitejavaddepally11@gmail.com>
2024-07-21 14:43:54 +0530

    added strategy pattern

---
+++ src/main/java/org/example/behavioural/strategy/CreditCard.java
@@ -0,0 +1,50 @@
+package org.example.behavioural.strategy;
+
+public class CreditCard {
+
+    private String number;
+    private String date;
+    private String cvv;
+    private ValidationStrategy validationStrategy;
+
+    public CreditCard(ValidationStrategy validationStrategy) {
+        this.validationStrategy = validationStrategy;
+    }
+
+    public boolean isValid(){
+
+        return validationStrategy.isValid(this);
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public ValidationStrategy getValidationStrategy() {
+        return validationStrategy;
+    }
+
+    public void setValidationStrategy(ValidationStrategy validationStrategy) {
+        this.validationStrategy = validationStrategy;
+    }
+}
